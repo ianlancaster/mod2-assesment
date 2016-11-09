24c2da79cd59d178385b89ff828db4122dc6a618
Ian Lancaster <ianclancaster@gmail.com>
2016-11-08 23:27:31 -0700

    passes all scoreWord tests

--- test/scripts.test.js
+++ test/scripts.test.js
@@ -2,7 +2,19 @@ import { expect } from 'chai'
 import { scoreWord } from '../lib/utilities/scripts'
 
 describe('scripts.js', () => {
-  it('what should it do', () => {
-    //
+  it('scoreWord("hello") should return 8', () => {
+    expect(scoreWord('hello')).to.equal(8)
+  })
+  it('scoreWord("") should return 0', () => {
+    expect(scoreWord('')).to.equal(0)
+  })
+  it('scoreWord(null) should return 0', () => {
+    expect(scoreWord(null)).to.equal(0)
+  })
+  it('scoreWord(" hello ") should return 8', () => {
+    expect(scoreWord(' hello ')).to.equal(8)
+  })
+  it('scoreWord("hello", 2) should return 16', () => {
+    expect(scoreWord('hello', 2)).to.equal(16)
   })
 })
